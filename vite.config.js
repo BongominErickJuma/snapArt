d89c05850debdcac7baa07a1c5479c1ca6b07491
BongominErickJuma <ericbongomin@gmail.com>
2024-11-15 07:46:30 +0300

    dc

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/snapArt",
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api/v1": {
-        target: "https://snapearn.online",
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
 });
